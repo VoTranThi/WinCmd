46e636fc1a57dfd9cc5afcf9761fd898e23cca80
Võ Trần Thi <mr.tranthi@gmail.com>
2025-07-16 09:09:54 +0700

    Create wingetList.bat

---
+++ wingetList.bat
@@ -0,0 +1,53 @@
+winget install Microsoft.AppInstaller
+
+winget install Microsoft.WindowsTerminal
+winget install Chocolatey.Chocolatey
+
+winget install RARLab.WinRAR
+winget install 7zip.7zip
+
+winget install Git.Git
+winget install GitHub.GitLFS
+winget install TortoiseGit.TortoiseGit
+winget install ShiningLight.OpenSSL.Light
+
+winget install Microsoft.msodbcsql.13
+winget install Microsoft.msodbcsql.17
+winget install Microsoft.VCRedist.2010.x64
+winget install Microsoft.CLRTypesSQLServer.2019
+winget install Microsoft.VCRedist.2008.x64
+winget install Oracle.JavaRuntimeEnvironment
+
+winget install Greenshot.Greenshot
+winget install Notepad++.Notepad++
+winget install AntibodySoftware.WizTree
+winget install voidtools.Everything
+winget install OBSProject.OBSStudio
+winget install DucFabulous.UltraViewer
+winget install FrankBijnen.ExifToolGUI
+winget install Telerik.Fiddler.Classic
+winget install Postman.Postman
+
+REM winget install ByteDance.Trae
+REM winget install PearAI.PearAI
+
+
+winget install Zoom.Zoom.EXE
+winget install Microsoft.Teams
+
+winget install Mozilla.Firefox
+winget install Google.Chrome
+winget install Microsoft.Edge
+REM winget install Brave.Brave
+winget install Google.GoogleDrive
+winget install Dropbox.Dropbox
+
+REM winget install Apple.iTunes
+REM winget install Apple.AppleMobileDeviceSupport
+REM winget install Apple.Bonjour
+winget install Daum.PotPlayer
+
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.VisualStudio.2022.Community
+winget install Python.Launcher
+winget install Python.Python.3.12
